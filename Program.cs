78e4585f11f06834388d5c18c2f9ee51827b0deb
Dmytro Grunytsky <dima.grunitsky@gmail.com>
2020-10-13 23:41:47 +0300

    Сума

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int count = int.Parse(Console.ReadLine ());
+            double result = 0;
+            for (int i = 1; i<count; i++)
+            {
+                result = result = 1D / i;
+            }
+            Console.WriteLine($"r={result}");
+        }
+    }
+}
